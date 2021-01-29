e5ab7b5f4d945758ba0b2e60c8da1a8cbab1efb4
Anastassios Nanos <ananos@nubificus.co.uk>
2021-01-29 12:30:23 +0000

    Call image classify

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: fileread.so test
 
 fileread.so:
-	gcc -Wall -c fileread.c -fPIC
+	gcc -Wall -c fileread.c -fPIC 
 	gcc -shared fileread.o -o libfileread.so
 	ar rcs libfileread.a fileread.o
 
 test:
-	gcc -Wall test.c -o test -lfileread -L.
-	gcc -Wall test.c -o test_static -static -lfileread -L.
+	gcc -Wall test.c -o test -lfileread -L. -I/usr/local/include
+	gcc -Wall test.c -o test_static -static -lfileread -L. -L/usr/local/lib -lvaccel
 
 clean:
 	rm -rf *.o *.so *.a test test_static
--- test.c
+++ test.c
@@ -7,8 +7,43 @@ int main(int argc, char ** argv)
 {
 	char *ptr;
 	ssize_t len;
+	int ret = 0;
 
 	fileread(&ptr, &len);
 	//fprintf(stderr, "ptr:%p len:%ld\n", ptr, len);
-	return 0;
+	char *image;
+       	size_t image_size;
+	char out_text[512], out_imagename[512];
+	struct vaccel_session sess;
+
+	ret = vaccel_sess_init(&sess, 0);
+	if (ret != VACCEL_OK) {
+		fprintf(stderr, "Could not initialize session\n");
+		return 1;
+	}
+
+	printf("Initialized session with id: %u\n", sess.session_id);
+
+	image = ptr;
+	image_size = len;
+
+	
+	ret = vaccel_image_classification(&sess, image, (unsigned char*)out_text, (unsigned char*)out_imagename, image_size, sizeof(out_text), sizeof(out_imagename));
+
+	if (ret) {
+		fprintf(stderr, "Could not run op: %d\n", ret);
+		goto close_session;
+	}
+
+	printf("classification tags: %s\n", out_text);
+
+close_session:
+	free(image);
+	if (vaccel_sess_free(&sess) != VACCEL_OK) {
+		fprintf(stderr, "Could not clear session\n");
+		return 1;
+	}
+	
+
+	return ret;
 }
